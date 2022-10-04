a45eddfa71e465c035ee7ded4e0a9043386c6e83
Paulo Vinicius Silva Costa <pauloviniciius@outlook.com>
2022-10-04 00:30:00 -0300

    [BACKEND] add JPA and H2 BD memory

--- backend/src/main/java/com/pauloviniciius/sales/domain/Categoria.java
+++ backend/src/main/java/com/pauloviniciius/sales/domain/Categoria.java
@@ -3,17 +3,25 @@ package com.pauloviniciius.sales.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
